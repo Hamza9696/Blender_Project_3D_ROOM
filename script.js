d76494f9c61472f30f4b98f760cd7deb5f2151f1
Hamza9696 <hamzayaich96@gmail.com>
2023-04-11 13:48:09 +0200

    style changes

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ gapi.load('client', function() {
 
     // Initialize the YouTube API client
     gapi.client.init({
-      apiKey: 'YOUR_API_KEY',  
+      apiKey: 'AIzaSyCyfAqW6DAjR9y0xRwCKhRka_uDGBYL4AM',  
       discoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest']
     }).then(function() {
       
       // Retrieve the description for the video
       gapi.client.youtube.videos.list({
         part: 'snippet',
-        id: 'YOUR_VIDEO_ID'
+        id: 'mtnWZ4ZtDos'
       }).then(function(response) {
         
         // Extract the description from the response
